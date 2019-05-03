152021d4b2fa52b84bba9054291b678b1cbac13a
adit <aeka489@gmail.com>
2019-05-03 22:04:00 +0700

    deploy mane bro

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
+const server = http.Server(app)
 
 server.listen(process.env.PORT || 3000, function () {
     console.log("server run at port " + port);
